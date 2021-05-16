27af05f82d3432890329a69ff652d073bf787da9
0x1a0b <65621431+0x1a0b@users.noreply.github.com>
2021-05-16 14:14:12 +0200

    customize color scheme for darkmode

--- src/pages/diagram.js
+++ src/pages/diagram.js
@@ -6,15 +6,15 @@ import './diagram.css';
 
 const initialSchema = createSchema({
     nodes: [
-        { id: 'node-1', content: 'Node 1', coordinates: [250, 60], },
-        { id: 'node-2', content: 'Node 2', coordinates: [100, 200], },
-        { id: 'node-3', content: 'Node 3', coordinates: [250, 220], },
-        { id: 'node-4', content: 'Node 4', coordinates: [400, 200], },
+        { id: 'node-1', content: 'Node 1', coordinates: [250, 60], disableDrag: true },
+        { id: 'node-2', content: 'Node 2', coordinates: [100, 200], disableDrag: true },
+        { id: 'node-3', content: 'Node 3', coordinates: [250, 220], disableDrag: true },
+        { id: 'node-4', content: 'Node 4', coordinates: [400, 200], disableDrag: true },
     ],
     links: [
         { input: 'node-1',  output: 'node-2', label: 'Link 1', readonly: true },
         { input: 'node-1',  output: 'node-3', label: 'Link 2', readonly: true },
-        { input: 'node-1',  output: 'node-4', label: 'Link 3', readonly: true, className: 'my-custom-link-class' },
+        { input: 'node-1',  output: 'node-4', label: 'Link 3', readonly: true },
     ]
 });
 
